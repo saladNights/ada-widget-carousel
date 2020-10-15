5d33315d46cd4261ecc1555db515141067f2a404
saladNights <>
2020-10-15 10:09:16 +0300

    Updates

---
+++ precache-manifest.bdb978961cbd8626f8b1e8de694fedfb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd5e2cb061002fc42031a0990194f7a",
+    "url": "/ada-widget-carousel/index.html"
+  },
+  {
+    "revision": "e8d66737e5bb42507e98",
+    "url": "/ada-widget-carousel/static/css/2.e5559355.chunk.css"
+  },
+  {
+    "revision": "83885016ca1a810773d5",
+    "url": "/ada-widget-carousel/static/css/main.48268aed.chunk.css"
+  },
+  {
+    "revision": "e8d66737e5bb42507e98",
+    "url": "/ada-widget-carousel/static/js/2.42175d7a.chunk.js"
+  },
+  {
+    "revision": "1ca5336a68bfe67001e8c21888e299b9",
+    "url": "/ada-widget-carousel/static/js/2.42175d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83885016ca1a810773d5",
+    "url": "/ada-widget-carousel/static/js/main.d3da5c72.chunk.js"
+  },
+  {
+    "revision": "6cecf5204ca83eed4723",
+    "url": "/ada-widget-carousel/static/js/runtime-main.6e5f0277.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ada-widget-carousel/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ada-widget-carousel/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ada-widget-carousel/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ada-widget-carousel/static/media/slick.f97e3bbf.svg"
+  }
+]);
