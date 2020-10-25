1b8c43d525531c760d0a671380fd5bd77cb9d019
saladNights <>
2020-10-25 13:28:23 +0300

    Updates

---
+++ precache-manifest.271c903371252ef3f80b94eb93d19972.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691dd7e89fecff4e71b48ddd9a9ea6a0",
+    "url": "/ada-widget-carousel/index.html"
+  },
+  {
+    "revision": "8e4444213c686ef26606",
+    "url": "/ada-widget-carousel/static/css/2.e5559355.chunk.css"
+  },
+  {
+    "revision": "07d2fa378cfdd1dbe50b",
+    "url": "/ada-widget-carousel/static/css/main.34d075d4.chunk.css"
+  },
+  {
+    "revision": "8e4444213c686ef26606",
+    "url": "/ada-widget-carousel/static/js/2.37ad816f.chunk.js"
+  },
+  {
+    "revision": "1ca5336a68bfe67001e8c21888e299b9",
+    "url": "/ada-widget-carousel/static/js/2.37ad816f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d2fa378cfdd1dbe50b",
+    "url": "/ada-widget-carousel/static/js/main.cc258c26.chunk.js"
+  },
+  {
+    "revision": "6cecf5204ca83eed4723",
+    "url": "/ada-widget-carousel/static/js/runtime-main.6e5f0277.js"
+  },
+  {
+    "revision": "c3fbefb0708fe2f326b29afd212df435",
+    "url": "/ada-widget-carousel/static/media/no-img.c3fbefb0.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ada-widget-carousel/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ada-widget-carousel/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ada-widget-carousel/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ada-widget-carousel/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7bf14eb836bb32a48ce2abc2e47eb59.js"
+  "/ada-widget-carousel/precache-manifest.271c903371252ef3f80b94eb93d19972.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ada-widget-carousel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
